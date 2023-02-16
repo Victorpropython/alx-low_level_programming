42338b7afaf9b9f24b962aa259b71e4eacd7bd2d
victorpropython <victorchibuzor3109@gmail.com>
2023-02-16 15:36:14 -0800

    advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *main - A G program that prints that piece of art is useful
+ *Return: 1(true)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\"-Dora korpar,2015-10-19\n", stdout);
+	return (1);
+}
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	char a;
-	int c;
-	float d;
-	long int f;
-	long long int g;
+	int b;
+	float c;
+	long int d;
+	long long int f;
 
-	printf("size of a char :%lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of an int :%lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of a float :%lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a long int :%lu byte(s)\n", (unsigned long)sizeof(f));
-	printf("size of a long long int :%lu byte(s)\n", (unsigned long)sizeof(g));
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int:  %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
