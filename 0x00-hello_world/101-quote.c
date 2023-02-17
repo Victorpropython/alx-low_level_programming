95a1c78fc3ba43f93731d9bbccbfe0284552620f
victorpropython <victorchibuzor3109@gmail.com>
2023-02-16 16:48:35 -0800

    for printout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  *main - A G program that prints that piece of art is useful
  *Return: Always 0 (true)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
+	write(1, "and that piece of art is useful
 			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
