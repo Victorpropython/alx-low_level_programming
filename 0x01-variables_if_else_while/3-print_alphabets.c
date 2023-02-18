5fe71815f99686560735b64f4a08502a35e72e62
victorpropython <victorchibuzor3109@gmail.com>
2023-02-18 06:42:59 -0800

    printing alphabets together

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+
+{
+	char ch;
+	char up;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
