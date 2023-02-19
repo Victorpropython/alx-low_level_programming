b326b3332e143baaec904b0b7b7d0fb577321a9a
victorpropython <victorchibuzor3109@gmail.com>
2023-02-18 19:45:17 -0800

    possible 3 digit combination without repetition

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - To print all possible three digit without repetion
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+				if (i != j && j != k && i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
