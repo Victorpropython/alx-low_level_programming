645647d83ff286f616ef21486c352451ebdef56c
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 07:55:53 -0800

    last digit of numbers

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - To print the last digit of a number
+ *@n: checker for last digit
+ * Return: Always 0
+ */
+int print_last_digit(int n)
+{
+	while (n % 10)
+	{
+	_putchar('n');
+	}
+	return (n);
+}
