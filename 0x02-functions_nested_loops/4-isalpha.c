cc9c9ce81e5b523ff408f6c3c00ccf0ca7970fc6
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 06:10:01 -0800

    commit for alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha(int c) - To check for alphabets
+ * @c: Used as variable for checker
+ *
+ * Return: Always 1 or 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' && c <= 'A' && c >= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
