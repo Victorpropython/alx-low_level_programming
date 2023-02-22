ed1e31550b8e57d028b38d3c138a1ab4786fa58f
victorpropython <victorchibuzor3109@gmail.com>
2023-02-21 20:47:43 -0800

    print alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to print alphabet 10 times
+ *
+ * Return: Always 0 success
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{_putchar(i);
+		for (i = 'a'; i <= 'z'; i++)
+		{_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
