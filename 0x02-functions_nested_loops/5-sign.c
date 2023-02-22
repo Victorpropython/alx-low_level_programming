4edc857132cd32aedf8b5e31eb446fb35f6c889c
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 06:50:06 -0800

    print sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - To print the sign of numbers
+ * @n: The checker for the numbers
+ *
+ * Return: can be positive negetive or zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{ _putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{ _putchar('-');
+		return (-1);
+	}
+	else
+	{ _putchar(0);
+		return (0);
+	}
+}
