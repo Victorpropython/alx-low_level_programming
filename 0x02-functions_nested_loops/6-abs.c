5936787631f4772cb323d8a47550fee401554ba0
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 07:33:48 -0800

    for absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - To get the absolute  of a number
+ *@n: checker for int
+ *@-n: value for negative number
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
+
+
