522175fbf68b12f90160330bb6099cf2f2d59cdc
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 12:37:59 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,3 +1,15 @@
 #include "main.h"
 /**
+ *times_table - to print the times table
  *
+ * Return: always zero
+ */
+void times_table(void)
+{
+	int j = n * t , i, g;
+
+	for (j = 0; j <= 9; j++)
+	{
+		while (t < 9)
+		{
+			_putchar((j / 10) + '0');
