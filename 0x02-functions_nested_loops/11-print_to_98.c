541039bad81eec73559f6970a1a94f24aeb7a8d7
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 20:15:48 -0800

    print numbers for a better coding

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,40 +3,21 @@
 
 /**
  * print_to_98 - to print first natural numbers
- *
  *@n: use as variable for natural numbers
  * Return: Always 0 suvvess
  */
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n >= 98)
 	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n--;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n >= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(",");
-			}
-			n++;
-		}
+		while (n > 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 	else
 	{
-		printf("98");
+		while (n < 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	printf("\n");
 }
-
