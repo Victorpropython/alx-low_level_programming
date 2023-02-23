c9529e32715be9d043787c09e980ff763c56f544
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 21:02:15 -0800

    commit for 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,3 +1,39 @@
 #include "main.h"
 /**
+ *print_times_table - To print times table from 1  to 15
  *
+ * Return: Always zero
+ */
+void print_times_table(int n)
+{
+	int b. m , j;
+
+	if (n <= 0 && n >= 15)
+	{
+
+	for (m = 0; m <= n; m++)
+	{ _putchar('0');
+		for (j = 1; j <= n; j ++)
+		{	_putchar(',');
+			_putchar(' ');
+
+			b = m * j;
+			if (b <= 99)
+				_putchar(' ');
+			if (b <= 9)
+				_putchar(' ');
+			if (b >= 100)
+			{
+				_putchar((b / 100) + '0');
+				_putchar(((b /100) % 10) + '0');
+			}
+			else (b <= 99 && b >= 10)
+			{
+				_putchar((b / 10) + '0');
+			}
+			_putchar((b % 10) + '0');
+		}
+	}
+	_putchar('\n');
+	}
+}	
