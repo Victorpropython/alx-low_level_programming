d2f67a254b271a5441d9ea1cfe56a757739c8b43
victorpropython <victorchibuzor3109@gmail.com>
2023-02-22 16:35:47 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,21 +18,16 @@ void times_table(void)
 			_putchar(' ');
 
 			b = n * t;
-			if ((b / 10) > 0)
+			if (b <= 9)
 			{
-				_putchar((b / 10) + '0');
+				_putchar(' ');
 			}
 			else
 			{
-				_putchar(' ');
+				_putchar((b / 10) + '0');
 			}
 			_putchar((b % 10) + '0');
-			if (t < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
 		}
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
 }
