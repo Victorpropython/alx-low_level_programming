d7097e5066381f62fe919eb15dd6ba579b724f59
victorpropython <victorchibuzor3109@gmail.com>
2023-02-23 20:36:45 -0800

    more commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - To multiply two integers
+ * @a: used as a variable
+ * @b: used as a variable
+ * Return: Alwas 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
