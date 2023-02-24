15d11cdbe2c102d531e6c339ed3b7989e2d63aa3
victorpropython <victorchibuzor3109@gmail.com>
2023-02-23 19:53:08 -0800

    my second commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - to check for digit
+ * @c: used as a var8able
+ * Return: can return 1 if true or 0 if false
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= 9);
+}
