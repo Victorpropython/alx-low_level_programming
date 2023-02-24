b277cb4a076a33039f34266dba7380cb0149aad3
victorpropython <victorchibuzor3109@gmail.com>
2023-02-23 20:58:20 -0800

    commit 2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_number - To print out digits
+ *Return: 0
+ */
+int print_number(void)
+{
+	int c;
+	
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
