ec01e75d90fb4f1c1de3693f51dcea67b977c780
victorpropython <victorchibuzor3109@gmail.com>
2023-02-24 17:12:27 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,15 +6,15 @@
  */
 void more_numbers(void)
 {
-	int i, c,  j;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j < 14; j++)
 		{
-			If (j > 9)
-				_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 	}
 	_putchar('\n');
