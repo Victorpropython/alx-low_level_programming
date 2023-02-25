f2d22257c9b3cc74447e830a5245e6d33c0dc0cc
victorpropython <victorchibuzor3109@gmail.com>
2023-02-24 16:36:53 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 void print_most_numbers(void)
 {
 	int c;
-	
+
 	for (c = 0; c <= 9; c++)
 	{
 		if (c != 2 && C != 4)
 		{
-			_putchar((c % 10) + '0');}
+			_putchar((c % 10) + '0');
 		}
 	}
 	_putchar('\n');
