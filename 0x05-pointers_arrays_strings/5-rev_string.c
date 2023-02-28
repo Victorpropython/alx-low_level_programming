24d6ec866dd85bbddeb60666c360a8460e147154
victorpropython <victorchibuzor3109@gmail.com>
2023-02-28 07:09:58 -0800

    to print string in reverse order

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *rev_string - To rverse a string
+ * @*s: use as a  vpionter variable
+ * Reeturn: Always 0
+ */
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+	len++;
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[len - 1];
+		s[len - index - 1] = temp;
+	}
+}
+
