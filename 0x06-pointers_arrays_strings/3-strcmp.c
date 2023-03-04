d096c8333ba315c0e07c3501cdc270694f396dc3
victorpropython <victorchibuzor3109@gmail.com>
2023-03-03 17:18:42 -0800

    to compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s2)
 
 	while (st == 0)
 	{
-		if (*(s1[i] == '\0') && *(s2[i] == '\0'))
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
-		st = *(s1[i]) - *(s2[i]);
+		st = *(s1 + i) - *(s2 + i);
 		i++;
 	}
 	return (st);
