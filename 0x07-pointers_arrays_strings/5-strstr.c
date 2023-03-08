eb43517946e9bec8e79f821a90a8018fb5b27f2a
victorpropython <victorchibuzor3109@gmail.com>
2023-03-07 18:59:22 -0800

    to locate a substring byte

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strstr - To locate a substring
+ * @haystack: pointer variable
+ * @needle: pointer variable
+ * Return: Always haystack
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *num1 = haystack;
+	char *num2 = needle;
+
+	for (; *haystack != '\0'; haystack++)
+	{
+		while (*num1 == *num2 && *num2 != '\0')
+		{
+			num1++;
+			num2++;
+		}
+		if (*num2 == '\0')
+			return (haystack);
+	}
+	return ('\0');
+}
