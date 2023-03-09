942c2a4d1a83cee8354f8b4347d97ce676cbd2d5
victorpropython <victorchibuzor3109@gmail.com>
2023-03-08 20:13:19 -0800

    for factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - To determine the factorial of a number
+ * @n: use as input variable
+ * Return: Always 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		if (n == 0)
+			return (1);
+	}
+	return (n * (factorial(n - 1)));
+}
+
