29f9f2b1fecbd0d779770f7d677ae45c8ca8df07
victorpropython <victorchibuzor3109@gmail.com>
2023-03-08 19:01:02 -0800

    to print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - To print a string in reverse
+ * @s: pionter variable for string
+ * Reyurn: Always 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
