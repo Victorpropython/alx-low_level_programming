8179ebaf9bad087f51b5118538dcaca6950a7e55
victorpropython <victorchibuzor3109@gmail.com>
2023-03-09 18:03:06 -0800

    comparing two string

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_palindrome - To print an empty string
+ * @s: char variable
+ * Return: 1 if palindrome or 0 of n9t
+ */
+int is_palindrome(char *s)
+{
+	char c;
+
+	if (*s != '\0')
+	{
+		return (0);
+	}
+	return (1);
+}
+
