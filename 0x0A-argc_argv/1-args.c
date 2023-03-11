4d2e9a23140aca612cf3601eca56327241a8fccd
victorpropython <victorchibuzor3109@gmail.com>
2023-03-10 20:55:41 -0800

    my firdt commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Tp print argument count
+ * @argc: used for argument count
+ * @argv; used for array of sisze argc
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0 ; i < argc; i++)
+	
+			printf("%d\n", i);
+	}
+	return (0);
+}
