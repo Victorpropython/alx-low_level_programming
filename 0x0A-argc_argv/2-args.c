07ad5d38820f50b06a8edfdf80126b3212e563c5
victorpropython <victorchibuzor3109@gmail.com>
2023-03-12 21:04:26 -0700

    for print argument followed by a new line task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - To print all argument given to it
+ * @argc: used as argument count
+ * @argv: used for array of size argc
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	if (argc > 1)
+	{
+		for ( ; i < argc; i++)
+				printf("%s\n", *argv++);
+	}
+	return 0;
+}
+
