88a98813065fdb9ea1689106086a9bc57e9d894e
victorpropython <victorchibuzor3109@gmail.com>
2023-06-20 06:17:36 +0100

    for intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  * main - To write out a message
  * Return: Always 1
 */
-
-int main() 
+int main(void)
 {
-    const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, msg, sizeof(msg) - 1);
-    return 1;
+	const char msg[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
 }
