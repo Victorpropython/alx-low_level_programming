b1f87d2244ed74dd455656ce784e4bd87d4d1762
victorpropython <victorchibuzor3109@gmail.com>
2023-06-21 03:44:41 +0100

    First commit for functions and nested loops

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ * _putchar - To print out character
+ * @c: the character to print
+ * Return: Always 1
+ * on error, Returns - 1 and errno is set in order
+ */
+int _putchar(char c)
+{
+	return (1, &c, -1);
+}
