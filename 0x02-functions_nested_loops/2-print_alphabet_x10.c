6ff22b161fe8386d77b4f31dc03389cce2a82210
victorpropython <victorchibuzor3109@gmail.com>
2023-06-21 05:41:59 +0100

    for 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char alpha = 'a';
 
-	for ( ; i < 10; i++)
+	while (i < 10)
 	{
-		while (alpha <= 'z')
+		for (; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 			alpha++;
