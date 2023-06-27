c4b64f1fa2fe01a37ed5cad76400376cbb5df18c
victorpropython <victorchibuzor3109@gmail.com>
2023-06-27 05:38:58 +0100

    for 0 task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *reset_to _98 - To reset int value to 98
+ *reset_to_98 - To reset int value to 98
  *@n: Used as pointer variable
  *
  *Return: Always (0) Success
  */
- void reset_to_98(int *n)
- {
- 	*n = 98;
- }
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
