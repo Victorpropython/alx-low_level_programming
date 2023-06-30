51d4c4062256f7489b6c378ce117ec59c1fa09ed
victorpropython <victorchibuzor3109@gmail.com>
2023-06-30 13:39:24 +0100

    for task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **string_toupper - To change all lowercase of string to upper
+ *
+ *Return: Always (0) success
+ */
+char *string_toupper(char *strg)
+{
+	int i;
+
+	for (i = 0; strg[i] != '\0'; i++)
+	{
+		if (strg[i] >= 'a' && strg[i] <= 'z')
+		{
+			strg[i] = strg[i] - 32;
+		}
+	}
+	return (strg);
+}
