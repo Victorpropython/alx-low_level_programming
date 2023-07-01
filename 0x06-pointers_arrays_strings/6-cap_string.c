610bb7d5659d5bbb2f32469ca64c2e1ea4619071
victorpropython <victorchibuzor3109@gmail.com>
2023-07-01 01:43:26 +0100

    for task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,7 +5,41 @@
  *
  *Return: Always (0) Success
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-	
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' '
+			|| str[i] == '\t'
+			|| str[i] == '\n'
+			|| str[i] == '.'
+			|| str[i] == ','
+			|| str[i] == '}'
+			|| str[i] == '{'
+			|| str[i] == '('
+			|| str[i] == ';'
+			|| str[i] == '!'
+			|| str[i] == '"'
+			|| str[i] == '?'
+			|| str[i] == ')')
+
+		{
+			i++;
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				i++;
+			else if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+			i--;
+		
+		}
+		else if (str[0] >= 'a' && str[0] <= 'z')
+		{
+			str[0] = str[0] - 32;
+		}
+	}
+	return (str);
 }
