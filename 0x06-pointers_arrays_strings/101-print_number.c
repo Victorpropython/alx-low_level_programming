e843c8a8204e2aaf61a14c324ba40099aa05b24b
victorpropython <victorchibuzor3109@gmail.com>
2023-07-01 03:26:29 +0100

    for task 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,12 +12,12 @@ void print_number(int n)
 		{
 			print_number(n / -10);
 		}
-		_putchar('0' -n % 10);
+		_putchar('0' - n % 10);
 	}
 	else
 	{
 		if (n > 9)
-			print_number(n /10);
+			print_number(n / 10);
 		_putchar(n % 10 + '0');
 	}
 }
