0633161d3536335e07945fa7a4cb1e93b49d216e
victorpropython <victorchibuzor3109@gmail.com>
2023-07-01 03:20:43 +0100

    for task 101

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_number - function to print integer
+ *@n: integer variable
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n < -9)
+		{
+			print_number(n / -10);
+		}
+		_putchar('0' -n % 10);
+	}
+	else
+	{
+		if (n > 9)
+			print_number(n /10);
+		_putchar(n % 10 + '0');
+	}
+}
