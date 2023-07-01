af03565332f2cb2bcbddb581511d72cf3a0330a8
victorpropython <victorchibuzor3109@gmail.com>
2023-07-01 02:38:35 +0100

    for task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ **leet - Function to encodes a string into 1337
+ *@str: pointer variable
+ *
+ *Return: To return encoded string
+ */
+char *leet(char *str)
+{
+	int v;
+	char vchar[] = {'4', '3', '0', '7', '1'};
+	char alpha[] = {'a', 'e', 'o', 't', 'l'};
+	char *ptr = str;
+
+	while (*str)
+	{
+		for (v = 0; v < 5; v++)
+		{
+			if (*str == alpha[v] || *str == alpha[v] - 32)
+				*str = vchar[v] + '0';
+		}
+		str++;
+	}
+	return (ptr);
+}
