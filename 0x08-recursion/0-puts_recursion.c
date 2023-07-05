76466d64d0177bb0d8542bc20cce37b165bb76ae
victorpropython <victorchibuzor3109@gmail.com>
2023-07-05 23:27:27 +0100

    for task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
+
 /**
- *_puts_recursion - To print a string
+ * _puts_recursion - To print a string
  *@s: pointer variable
  *
  *Return: Always 0 (success)
  */
+
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if(*s == '\0')
 	{
-		_putchar(s[i]);
+		_putchar('\n');
+		return(0);
 	}
 	_putchar('\n');
+	_puts_recursion(s + 1);
 }
