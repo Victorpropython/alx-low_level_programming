ab4921937635f18f93f5ab791399302973fac9c0
victorpropython <victorchibuzor3109@gmail.com>
2023-07-16 22:41:39 +0100

    my first commit for task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	array = (char *)malloc((size + 1) * sizeof(char));
+	array = malloc((size) * sizeof(char));
 
 	if (array == NULL)
 	{
