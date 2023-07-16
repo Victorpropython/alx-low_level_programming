9c1adb88cf64ed5d12bb485649e65871ab67565e
victorpropython <victorchibuzor3109@gmail.com>
2023-07-16 14:42:38 +0100

    for task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,40 +5,26 @@
  * main -  a program that adds positive numbers.
  *@argc: for argument count
  *@argv: for array of argument
- *
+ *if the character is not a digit, it print "Error" & return 1.
+ *if the character is a digit, the prgram adds the number to the sum
  *Return: Always 0 success
  */
 int main(int argc, char *argv[])
 {
-	int sum, i, j, num;
-	char *arg;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int sum = 0, i, j;
 
 	for (i = 1; i < argc; i++)
 	{
-		arg = argv[i];
-
-		for (j = 0; arg[j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(arg[j]))
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		num = atoi(arg);
+		sum += atoi(argv[i]);
 		
-		if (num <= 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += num;
 	}
 	printf("%d\n", sum);
 	return (0);
