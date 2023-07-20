631e446ab5b7f7e5e126a4473e6c714b5f155b8a
victorpropython <victorchibuzor3109@gmail.com>
2023-07-20 04:42:01 +0100

    for task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_name - function to print a name
+ *@name: pointer to variable
+ *@f: pointer to function to print name
+ *
+ *Return: Always 0 success.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
+
+
