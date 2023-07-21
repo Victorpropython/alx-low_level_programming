48e230011bb3bb89e580f8f8c889142eec47ee52
victorpropython <victorchibuzor3109@gmail.com>
2023-07-21 06:00:31 +0100

    for task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,10 +4,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
-
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);void print_all(const char * const format, ...);
-int putchar(char c);
+int _putchar(char c);
 
 #endif
