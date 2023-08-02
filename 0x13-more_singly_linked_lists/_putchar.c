5ce01bfb398e6da6660c102dd0326bddfb9547be
Victorpropython <Victorchibuzor3109@gmail.com>
2023-08-02 02:53:12 +0100

    for _putchar and lists.h header file

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *_putchar - To print a character
+ *@c: Character variable
+ *
+ *Return: To return 1 on Success or -1 on failure
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
