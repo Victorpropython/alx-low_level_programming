624fff5b9a986bf8df14b35afdecc0d331b544b2
Victorpropython <victorchibuzor3109@gmail.com>
2023-09-15 11:13:45 +0100

    for variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,13 +10,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-/**
+
 typedef struct printer_s
 {
 	char *symbol;
 	void (*print)(va_list arg);
 } printer_t;
-**/
+
 void print_char(va_list arg);
 void print_float(va_list arg);
 void print_int(va_list arg);
