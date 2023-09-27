a01b11d4dc8ae4d8e8d76a6627e52a9f521ba4ad
victorpropython <victorchibuzor3109@gmail.com>
2023-09-27 05:10:11 +0100

    for task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * delete_nodeint_at_index -function that deletes the node at index 
+ * delete_nodeint_at_index -function that deletes the node at index
  * @index: is the index of the node that should be deleted.
  * @head: Head of node
  *
@@ -11,8 +11,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *tmp = *head;
 	listint_t *old = NULL;
-	
-	if (*head ==NULL)
+
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
@@ -32,5 +32,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	old->next = tmp->next;
 	free(tmp);
-	return(1);
+	return (1);
 }
