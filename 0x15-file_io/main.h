144f72cb7d22cee6f700dc66c4d6a1170d2e02a0
victorpropython <victorchibuzor3109@gmail.com>
2023-10-31 01:36:38 +0100

    for task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <fcnt1.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 
 int append_text_to_file(const char *filename, char *text_content);
