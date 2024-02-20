75455fb7ac2db47e6696d275c5b6492cb14789cc
victorpropython <victorchibuzor3109@gmail.com>
2024-02-20 15:49:05 +0100

    for task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * insert_dnodeint_at_index - To insert a new node at a given index
- * @h: Used as pointer to pointer variable 
+ * @h: Used as pointer to pointer variable
  * @idx: Index number
  * @n: the number that is position
  * Return: to return address of node or NULL
@@ -10,16 +10,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *ptr = *h, *newP = NULL;
 	unsigned int i;
-		
+
 	newP = malloc(sizeof(dlistint_t));
 	if (newP == NULL)
 		return (NULL);
-	newP->n =n;
+	newP->n = n;
 	newP->prev = NULL;
 	newP->next = NULL;
-
 	if (n == 0)
-	{
 		newP->next = *h;
 		if (*h != NULL)
 		{
@@ -27,7 +25,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		*h = newP;
 		return (newP);
-	}
 	for (i = 0; i < idx - 1; i++)
 	{
 		if (ptr == NULL)
@@ -38,10 +35,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		ptr = ptr->next;
 	}
 	if (ptr == NULL)
-	{
 		free(newP);
 		return (NULL);
-	}
 	newP->next = ptr->next;
 	if (ptr->next != NULL)
 	{
