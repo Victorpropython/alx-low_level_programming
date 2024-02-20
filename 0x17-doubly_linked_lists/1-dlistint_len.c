2caf168e717f171982d9092f278ecfa909e82af5
victorpropython <victorchibuzor3109@gmail.com>
2024-02-20 14:15:30 +0100

    for task 6

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
  * dlistint_len - To print the numbers in a linked list
- * @h: Used as pointer 
- * REturn: TO return the number of elements in a linked list
+ * @h: Used as pointer
+ * Return: TO return the number of elements in a linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	int ptr = 0;
 	const dlistint_t *thead = h;
-	
+
 	while (thead != NULL)
 	{
 		ptr++;
