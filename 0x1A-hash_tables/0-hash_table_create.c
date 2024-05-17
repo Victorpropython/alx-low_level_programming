894c3259837e13ff09a9b3513b402db422bb4ae0
victorpropython <victorchibuzor3109@gmail.com>
2024-05-17 08:48:16 +0100

    fr task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,27 +10,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ptr;
-	/*unsigned long int i;*/
 
-	ptr = malloc(sizeof(hash_table_t));
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
+	ptr = calloc(1, sizeof(hash_table_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
 	(*ptr).size = size;
-
-	(*ptr).array = malloc(size * sizeof(hash_node_t));
+	(*ptr).array = calloc(size, sizeof(hash_node_t));
 	if ((*ptr).array == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	/*for (i = 0; i < size; i++)
-	*{
-	*	(*ptr).array[i] = NULL;
-	*}
-	*/
-
 	return (ptr);
 }
